0cfd76c6228ee3a09f3680d54c6902c114ea8128
Tim Martin <timplode@gmail.com>
2025-12-18 16:02:15 -0600

    more initialization

--- mysql-init/01-init-ues-database.sql
+++
@@ -1,8 +0,0 @@
--- Create database if not exists
-CREATE DATABASE IF NOT EXISTS `ues`;
-
--- Grant all privileges on ues database to root from any host
-GRANT ALL PRIVILEGES ON `ues`.* TO 'dev'@'%';
-
--- Flush privileges to ensure changes take effect
-FLUSH PRIVILEGES;
